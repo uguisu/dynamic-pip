2b9ebbd2e0c1d004e976dc05c5e54be08f4edae4
uguisu <uguisu@users.noreply.github.com>
2022-10-11 00:46:24 +0800

    update: invite setup file

---
+++ setup.py
@@ -0,0 +1,84 @@
+# coding=utf-8
+# author xin.he
+from dynamicPip import __version__
+from setuptools import setup, find_packages
+
+import sys
+
+
+def long_description_load(filename):
+    """
+    long description loader
+    """
+    try:
+        with open(filename, mode='r', encoding='utf-8') as f:
+            _long_description = f.read()
+    except FileNotFoundError:
+        _long_description = ''
+
+    return _long_description
+
+
+def parse_requirements(filename):
+    """
+    load requirements from a pip requirements file. (replacing from pip.req import parse_requirements)
+    """
+    lines = (line.strip() for line in open(filename))
+    return [line for line in lines if line and not line.startswith("#")]
+
+
+# verify python version
+if sys.version_info < (3, 6, 0):
+    raise OSError(f'dynamic-pip requires Python >=3.6, but yours is {sys.version}')
+
+
+pkg_name = 'dynamic-pip'
+long_description = long_description_load('README.md')
+reqs = parse_requirements('requirements.txt')
+
+setup(
+    name=pkg_name,
+    version=__version__,
+    author='xin.he',
+    author_email='xin.he@dynamic-pip.com',
+    url='https://github.com/uguisu/dynamic-pip',
+    download_url='https://github.com/uguisu/dynamic-pip/tags',
+    python_requires='>=3.6',
+    license='Apache 2.0',
+    description='Help users to dynamically manage python package resources',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    zip_safe=False,
+    setup_requires=['setuptools>=18.0', 'wheel'],
+    install_requires=reqs,
+    include_package_data=True,
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Unix Shell',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'dynamic-pip = dynamicPip.dync_pip.__main__:exec'
+        ]
+    },
+
+    project_urls={
+        'Documentation': 'https://github.com/uguisu/dynamic-pip',
+        'Source': 'https://github.com/uguisu/dynamic-pip',
+        'Tracker': 'https://github.com/uguisu/dynamic-pip/issues',
+    },
+
+    keywords='dynamic pip packages',
+)
