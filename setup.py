ec8786d418a9b3d9e54ec282265c74129d54f60b
uguisu <uguisu@users.noreply.github.com>
2022-10-30 00:46:25 +0800

    update: fix README

--- setup.py
+++ setup.py
@@ -24,7 +24,34 @@ def long_description_load(filename):
     except FileNotFoundError:
         _long_description = ''
 
-    return _long_description
+    RAW_LOGO = 'assets/logo.jpg'
+    PYPI_LOGO = 'https://raw.githubusercontent.com/uguisu/dynamic-pip/main/assets/logo.jpg'
+    RAW_MERMAID = '```mermaid'
+    PYPI_MERMAID = '![mermaid](https://raw.githubusercontent.com/uguisu/dynamic-pip/main/assets/dependence_tree.jpg)'
+
+    # remove logo
+    _long_description = _long_description.replace(RAW_LOGO, PYPI_LOGO)
+    
+    rtn = []
+    is_mermaid_area = False
+    for ln in _long_description.splitlines(keepends=False):
+
+        if is_mermaid_area:
+            if ln == '```':
+                # rollback flag
+                is_mermaid_area = False
+                # use an image instead
+                rtn.append(PYPI_MERMAID)
+            continue
+
+        if (not is_mermaid_area) and (RAW_MERMAID == ln):
+            # find mermaid area
+            is_mermaid_area = True
+            continue
+
+        rtn.append(ln)
+
+    return '\n'.join(rtn)
 
 
 def parse_requirements(filename):
